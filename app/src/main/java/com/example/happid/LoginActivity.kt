7795ede7e8a0eebd4fa53c7a2791178656339965
NamanKumar2501 <namankumar246727@gmail.com>
2023-10-27 01:00:29 +0530

    OTP login verification

--- app/src/main/java/com/example/happid/LoginActivity.kt
+++ app/src/main/java/com/example/happid/LoginActivity.kt
@@ -1,11 +1,64 @@
 package com.example.happid
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.Window
+import android.widget.TextView
+import android.widget.Toast
+import com.example.happid.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        binding.ivBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+        binding.llOtpButton.setOnClickListener {
+            val countryCode: String = binding.countryCode.selectedCountryCode
+            val phoneNo = binding.etPhoneNumber.text.toString()
+            if (phoneNo.length < 10){
+                Toast.makeText(this, "Invalid Phone Number", Toast.LENGTH_SHORT).show()
+            }else{
+                val startTwoDigits = phoneNo.substring(0, 2)
+                val endTwoDigits = phoneNo.substring(phoneNo.length - 2)
+                otp(startTwoDigits, endTwoDigits, phoneNo, countryCode)
+            }
+        }
+
+    }
+
+    private fun otp(a: String, endTwoDigits: String, phoneNo: String, countryCode: String) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.otp_dialog)
+        dialog.setCancelable(true)
+
+        val otpText = dialog.findViewById<TextView>(R.id.txOTP)
+        val otp = a + endTwoDigits
+        otpText.text = otp
+
+        dialog.show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OTPActivity::class.java)
+            intent.putExtra("phoneNo", "+$countryCode $phoneNo")
+            intent.putExtra("otp", otp)
+            startActivity(intent)
+            dialog.dismiss()
+        }, 2000)
+
     }
 }
---
+++ app/src/main/java/com/example/happid/OTPActivity.kt
@@ -0,0 +1,45 @@
+package com.example.happid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.happid.databinding.ActivityLoginBinding
+import com.example.happid.databinding.ActivityOtpactivityBinding
+
+class OTPActivity : AppCompatActivity() {
+
+    private lateinit var otpBinding: ActivityOtpactivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        otpBinding = ActivityOtpactivityBinding.inflate(layoutInflater)
+        val view: View = otpBinding.root
+        setContentView(view)
+
+        val phoneNo: String = intent.getStringExtra("phoneNo").toString()
+        val otp: String = intent.getStringExtra("otp").toString()
+
+        otpBinding.txPhoneNumber.text = phoneNo
+
+        otpBinding.ivBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        otpBinding.editPhone.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        otpBinding.llOtpSubmit.setOnClickListener {
+            val inputOtp = otpBinding.pinview.text.toString()
+            if (inputOtp == otp) {
+                val intent = Intent(this, CreateProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "In-Correct OTP!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
